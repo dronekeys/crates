978ce249bba8a55e745b239a506b9fca860768fd
Andrew Symington <andrew.c.symington@gmail.com>
2014-06-10 16:02:39 +0100

    Reverted to source based gazebo install for DEM support

--- thirdparty/install_gazebo.sh
+++ thirdparty/install_gazebo.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-# Add to apt
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu trusty main" > /etc/apt/sources.list.d/gazebo-latest.list'
+# Save the base directory
+BASEDIR=${PWD}
 
-# Add the key
-wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+# Install sdformat
+cd $BASEDIR
+if [[ ! -d $BASEDIR/sdformat ]]; then
+	hg clone https://bitbucket.org/osrf/sdformat
+fi
+cd sdformat
+hg up sdf_2.0
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Debug ../
+make -j4
+sudo make install
 
-# Update the libraries
-sudo apt-get update
-
-# Install base libraries
-sudo apt-get -y install libgazebo-dev
+# Install gazebo
+cd $BASEDIR
+if [[ ! -d $BASEDIR/gazebo ]]; then
+	hg clone https://bitbucket.org/osrf/gazebo
+fi
+cd gazebo
+hg up gazebo_3.0
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DENABLE_TESTS_COMPILATION:BOOL=False -DCMAKE_BUILD_TYPE=Debug ../
+make -j4
+sudo make install
 
 # Create a home directory or gazebo wont start
 mkdir -p ~/.gazebo
