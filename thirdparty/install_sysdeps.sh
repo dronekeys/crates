0228e27788899392e430a65c6256fb4d488371d7
Andrew Symington <andrew.c.symington@gmail.com>
2014-06-10 15:07:32 +0100

    Added java to system dependencies

--- thirdparty/install_sysdeps.sh
+++ thirdparty/install_sysdeps.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Add oracle java support
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+
 # Basic libraries
 sudo apt-get -y install gradle build-essential doxygen libtinyxml2-dev libkml-dev libboost-all-dev cmake mercurial pkg-config \
  	libprotoc-dev libprotobuf-dev protobuf-compiler libqt4-dev libtar-dev \
  	libcurl4-openssl-dev libcegui-mk2-dev libopenal-dev libtbb-dev \
  	libswscale-dev libavformat-dev libavcodec-dev libogre-1.8-dev libgts-dev libltdl3-dev \
- 	playerc++ libxml2-dev libfreeimage-dev freeglut3-dev libgeographiclib-dev
+ 	playerc++ libxml2-dev libfreeimage-dev freeglut3-dev libgeographiclib-dev oracle-java8-installer
+
