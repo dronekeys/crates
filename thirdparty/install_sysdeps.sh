5071da8be0b8ef7ec77a1ae9f6d7b315bd7c908a
Gregory Lyras <greglyras@gmail.com>
2014-07-09 12:03:06 +0100

    yes please install java

--- thirdparty/install_sysdeps.sh
+++ thirdparty/install_sysdeps.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 
 # Basic libraries
-sudo apt-get -y install gdal-bin libgdal-dev python-gdal gradle build-essential \
+sudo apt-get -qq -y install gdal-bin libgdal-dev python-gdal gradle build-essential \
     doxygen libtinyxml2-dev libkml-dev libboost-all-dev cmake mercurial pkg-config \
  	libprotoc-dev libprotobuf-dev protobuf-compiler libqt4-dev libtar-dev \
  	libcurl4-openssl-dev libcegui-mk2-dev libopenal-dev libtbb-dev \
